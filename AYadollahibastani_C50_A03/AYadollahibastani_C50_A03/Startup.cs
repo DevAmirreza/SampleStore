cd4492637d679525f1ba48bc44012986dec67a78
Amirreza <atnniya@gmail.com>
2017-11-05 11:46:48 -0500

    Initializing project

---
+++ AYadollahibastani_C50_A03/AYadollahibastani_C50_A03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AYadollahibastani_C50_A03.Startup))]
+namespace AYadollahibastani_C50_A03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
